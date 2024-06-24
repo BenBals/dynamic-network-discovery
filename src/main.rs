296ef48543fbc05c569f54da2a0cc8b8a2c5999e
Ben Bals <benbals@posteo.eu>
2024-06-24 12:10:05 +0200

    more deltas!

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,7 @@ const TMAX_FACTORS: [f64; 26] = [
     3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
 ];
 
-const DELTA_FACTORS: [f64; 2] = [0.01, 0.5];
+const DELTA_FACTORS: [f64; 8] = [0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5];
 
 #[derive(Debug, Clone)]
 struct ExperimentTask {
