9fa6d01f99cebe38e784b271314f6cef7a171fca
Ben Bals <benbals@posteo.eu>
2024-06-24 16:04:47 +0200

    test less graphs

--- src/main.rs
+++ src/main.rs
@@ -57,15 +57,15 @@ fn write_results(results: &Vec<ExperimentResult>) -> Result<(), Box<dyn Error>> 
     Ok(())
 }
 
-const MAX_NODES: usize = 200;
-const NODES_STEP_SIZE: usize = 10;
-const REPEATS: usize = 25;
+const MAX_NODES: usize = 100;
+const NODES_STEP_SIZE: usize = 5;
+const REPEATS: usize = 10;
 const PROBABILITIES: [f64; 12] = [
     0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5, 0.7, 0.9,
 ];
-const TMAX_FACTORS: [f64; 19] = [
-    0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.5, 2.0, 2.5,
-    3.0, 5.0, 7.0, 9.0, 10.0,
+const TMAX_FACTORS: [f64; 14] = [
+    0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.9, 1.0, 2.0,
+    3.0, 5.0, 7.0, 10.0,
 ];
 
 const DELTA_FACTORS: [f64; 6] = [0.0, 0.01, 0.05, 0.1, 0.3, 0.5];
